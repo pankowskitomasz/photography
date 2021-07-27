c3b96bc9e71f594ce244c00808c29631b3fd7740
pankowskitomasz <pankowski_tomasz@o2.pl>
2021-07-27 22:11:58 +0200

    frontend: contact, login, register

---
+++ src/views/blog.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class Blog extends Component{
+    render(){
+        return(          
+            <main className="minh-100vh">    
+            
+            </main>
+        );
+    }
+}
+
+export default Blog;
---
+++ src/views/classes.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class Classes extends Component{
+    render(){
+        return(          
+            <main className="minh-100vh">    
+            
+            </main>
+        );
+    }
+}
+
+export default Classes;
